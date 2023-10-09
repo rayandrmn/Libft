b75634adae87fd446d427d9b874c0dbb9639f749
rayan <rayan.drmn@gmail.com>
2023-10-09 11:06:14 +0200

    ft_bzero

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rayandrmn <rayandrmn@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 12:07:50 by rayderha          #+#    #+#             */
-/*   Updated: 2023/10/06 13:36:03 by rayandrmn        ###   ########.fr       */
+/*   Updated: 2023/10/09 10:57:09 by rayandrmn        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ int	ft_isascii(int c);
 int	ft_isprint(int c);
 void    *ft_memset(void *s, int c, unsigned int len);
 size_t  ft_strlen(const char *str);
+void    ft_bzero(void *s, size_t n);
 
 #endif
