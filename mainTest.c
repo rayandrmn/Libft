24786c3cd78a822b44faaf8af67126f83d4793fe
rayandrmn <rayan.drmn@gmail.com>
2023-10-09 11:23:40 +0200

    la clee ssh marche ?

--- mainTest.c
+++ mainTest.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mainTest.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rayderha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rayderha <rayandrmn@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 12:18:22 by rayderha          #+#    #+#             */
-/*   Updated: 2023/10/05 14:13:08 by rayderha         ###   ########.fr       */
+/*   Updated: 2023/10/06 13:07:08 by rayandrmn        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,42 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <unistd.h>
+#include <strings.h>
 
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	
+}
 
 int	main(void)
 {
-	const char string[] = "test";
-	printf("%ld\n", strlen(string));
-	printf("%ld", ft_strlen(string));
+	char string[] = "test";
+	char stringdeux[] = "test";
+	ft_bzero(string, 2);
+	for(int i = 0; i < 4; i++)
+	{
+		if (string[i] == 0)
+			printf("/0");
+		else
+			printf("%c", string[i]);
+	}
+	printf("\n");
+	bzero(stringdeux, 2);
+	for(int i = 0; i < 4; i++)
+	{
+		if (stringdeux[i] == 0)
+			printf("/0");
+		else
+			printf("%c", stringdeux[i]);
+	}
 	return (0);
 }
