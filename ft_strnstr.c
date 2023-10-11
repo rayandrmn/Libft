154370e2836c73e473ea19e29538a89b0b69662e
rayandrmn <rayan.drmn@gmail.com>
2023-10-11 15:26:26 +0200

    strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rayderha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 11:19:45 by rayderha          #+#    #+#             */
+/*   Updated: 2023/10/11 15:25:58 by rayderha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *s1, const char *s2, size_t n)
+{
+	int		i;
+	size_t	len;
+
+	i = 0;
+	len = ft_strlen(s2);
+	if (*s1 == 0 || *s2 == 0)
+		return ((char *)s1);
+	while (*s1 && n-- > 0)
+	{
+		if (*s1 == *s2)
+		{
+			while (*s1)
+			{
+				if (i == (len - 1))
+					return ((char *)s1);
+				if (*s1 != s2[i++])
+					break ;
+				s1++;
+			}
+		}
+		s1++;
+	}
+	return ((void *)0);
+}
